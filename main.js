7b495ea47e456073974a260d7d5dcc8bf028f692
Sebastián Enrique Becerra Cancino <sebaebc@Sebastians-MacBook-Pro.local>
2026-06-06 12:51:09 -0400

    feat: agrega funciones para obtener data desde API via axios

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
-console.log('Hola Mundo');
+async function getDataExampleFetch() {
+  const response = fetch("https://jsonplaceholder.typicode.com/posts/1")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
+
+async function getDataExampleAxios() {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts/1",
+  );
+
+  console.log(response.data);
+}
+
+async function getDataExampleAll() {
+  const response = await axios
+    .get("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => {
+      console.log(response.data);
+      showDataExampleAll(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+    .finally(() => {
+      console.log("Request completed");
+    });
+}
+
+async function showDataExampleAll(posts) {
+  let postsTable = document.querySelector(".js-posts tbody");
+
+  posts.forEach((post) => {
+    let row = document.createElement("tr");
+    row.innerHTML = `
+          <th scope="row">${post.id}</th>
+          <td>${post.title}</td>
+        `;
+
+    postsTable.appendChild(row);
+  });
+}
+
+console.log("Hola Mundo");
